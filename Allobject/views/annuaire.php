337c0fe5a8e7209a592fef65b42fe314dba018bb
Killian <killianbchr@gmail.com>
2021-04-15 13:07:53 +0200

    Added routes + php views

---
+++ Allobject/views/annuaire.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "annuaire bonjour";
--- public/index.php
+++ public/index.php
@@ -13,18 +13,9 @@ $uri = $_SERVER['REQUEST_URI'];
 $router = new AltoRouter();
 
 $router->map('GET', '/', 'home');
-$router->map('GET', '/contact', 'contact', 'contact');
-$router->map('GET', '/blog/[*:slug]-[i:id]', 'blog/article', 'article');
-$router->map('GET', '/user/[i:id]', 'user', 'user');
-$router->map('GET', '/user/update/[i:id]', 'userUpdate', 'userUpdate');
-$router->map('GET', '/collection/[i:id]', 'collection', 'collection');
-$router->map('GET', '/managemypools', 'managemypools', 'managemypools');
-$router->map('GET', '/create', 'create', 'create');
-$router->map('POST', '/create', 'created', 'created');
-$router->map('GET', '/connexion', 'connexion', 'connexion');
-$router->map('GET', '/edit/[i:id]', 'edit', 'edit');
-$router->map('POST', '/edit/[i:id]', 'edited', 'edited');
-$router->map('GET', '/search', 'search', 'search');
+$router->map('GET', '/conseils', 'conseils', 'conseils');
+$router->map('GET', '/annuaire', 'annuaire', 'annuaire');
+$router->map('GET', '/avis', 'avis', 'avis');
 $router->map('GET', '/faq', 'faq', 'faq');
 $match = $router->match();
 
