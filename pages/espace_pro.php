03b3830191cf0d1f0f9f42b5d5d6b1ecf4a3af7d
Killian <killianbchr@gmail.com>
2021-04-29 16:29:59 +0200

    Vérif login - à retoucher

--- pages/espace_pro.php
+++ pages/espace_pro.php
@@ -6,55 +6,57 @@ $menu = require($_SERVER['DOCUMENT_ROOT'] . "/src/menu.php");
 
 if (isset($_SESSION['utilisateur'])) {
     echo '
-    <form method="post" action="/logout">
-        <input type="submit" value="Déconnexion" name="but_logout">
-    </form>
+        <form method="post" action="/logout">
+            <input type="submit" value="Déconnexion" name="but_logout">
+        </form>
     ';
 } else {
     echo '
-    <div class="container">
-    <form id="form_login" method="post" action="/login"">
-        <div id="div_login">
-            <div>
-                <input type="text" class="textbox" id="email" name="email" placeholder="E-mail" />
+        <div class="container">
+        <form id="form_login" method="post" action="/login"">
+            <div id="div_login">
+                <div>
+                    <input type="text" class="textbox" id="email" name="email" placeholder="E-mail" />
+                </div>
+                <div>
+                    <input type="password" class="textbox" id="password" name="password" placeholder="Mot de passe"/>
+                </div>
+                <div>
+                    <input type="submit" value="Se connecter" name="but_submit" id="but_submit" />
+                </div>
             </div>
-            <div>
-                <input type="password" class="textbox" id="password" name="password" placeholder="Mot de passe"/>
-            </div>
-            <div>
-                <input type="submit" value="Se connecter" name="but_submit" id="but_submit" />
-            </div>
+        </form>
         </div>
-    </form>
-    </div>
     ';
 
     echo '<br><br>';
 
     echo '
-    <form method="post" id="inscription" action="/espace_pro">
+        <form method="post" action="/espace_pro">
 
-        <input name="email" id="email" placeholder="E-mail" type="email" required>
-        <div id="texte_erreur_email"></div>
-        <input name="mot_de_passe" id="mot_de_passe" placeholder="Mot de passe" type="password">
+            <input name="email_inscription" id="email_inscription" placeholder="E-mail" type="email" required>
 
-        <input name="mot_de_passe_verif" id="mot_de_passe_verif" placeholder="Veuillez retapez votre mot de passe" type="password">
-        <div id="texte_erreur_mdp"></div>
+            <input name="mot_de_passe" id="mot_de_passe" placeholder="Mot de passe" type="password">
 
-        <input name="nom_entreprise" id="nom_entreprise" placeholder="Nom de l\'entreprise" type="text">
+            <input name="mot_de_passe_verif" id="mot_de_passe_verif" placeholder="Veuillez retapez votre mot de passe" type="password">
+            <div id="texte_erreur_mdp"></div>
 
-        <input name="siren" id="siren" placeholder="SIREN" type="text">
+            <input name="nom_entreprise" id="nom_entreprise" placeholder="Nom de l\'entreprise" type="text">
 
-        <input name="adresse" id="adresse" placeholder="Adresse de l\'entreprise" type="text">
+            <input name="siren" id="siren" placeholder="SIREN" type="text">
 
-        <input name="code_postal" id="code_postal" placeholder="Code postal" type="text">
+            <input name="adresse" id="adresse" placeholder="Adresse de l\'entreprise" type="text">
+
+            <input name="code_postal" id="code_postal" placeholder="Code postal" type="text">
         
-        <input name="ville" id="ville" placeholder="Ville" type="text">
+            <input name="ville" id="ville" placeholder="Ville" type="text">
     
-        <input name="telephone" id="telephone" placeholder="N° de téléphone" type="text">
+            <input name="telephone" id="telephone" placeholder="N° de téléphone" type="text">
     
-        <input type="submit" value="S\'inscrire" />
+            <input id="inscription" type="submit" value="S\'inscrire" />
         
-    </form>
+        </form>
+
+    <div id="resultat"></div>
     ';
 }
--- src/scripts/espace_pro.js
+++ src/scripts/espace_pro.js
@@ -1,35 +1,49 @@
 $(document).ready(function (e) {
-  $("#inscription").submit(function (e) {
+  $("#inscription").click(function (e) {
     e.preventDefault();
-    /*if(verification_Email()){
-      $("#texte_erreur_email").text("Cet email possède déjà un compte. Veuillez vous connectez avec ou en utiliser une autre.");
+    console.log($("#email_inscription").val())
+    if (!verification_email()) {
       e.preventDefault();
-    }*/
+    }
     if ($("#mot_de_passe").val() != $("#mot_de_passe_verif").val()) {
       $("#texte_erreur_mdp").text("Les mots de passe ne correspondent pas.");
       e.preventDefault();
     }
-
-    var email = $("#email").val();
-    $.ajax({
-      url: '../../functions/verification_email.php',
-      type: 'post',
-      data: {
-        'email':email,
-        'email_check':1,
-    },
-
-    success:function(response) {	
-      $("#email_error").remove();
-      $("#email").after("<span id='email_error' class='text-danger'>"+response+"</span>");
-    },
-
-    error:function(e) {
-      $("#result").html("Something went wrong");
-    }
-  });
-
-
-
   });
 });
+
+function verification_email() {
+  var email = $("#email_inscription").val();
+  var error = false;
+  if (email != ""){
+    $.ajax({
+      url: "/functions/verification_email.php",
+      type: "post",
+      async: false,
+      data: {
+        email: email,
+        email_check: 1,
+      },
+      success: function (response) {
+        $("#email_erreur").remove();
+        if(response === "") {
+          error = true;
+        } 
+        else {
+          $("#email_inscription").after(
+            "<span id='email_erreur' class='text-danger'>" + response + "</span>"
+          );
+          error = false;
+        }
+      },
+      error: function (e) {
+        $("#resultat").html(
+          "Quelque chose ne fonctionne pas correctement. Veuillez réessayer plus tard."
+        );
+      },
+    });
+  }
+  
+  
+  return error;
+}
