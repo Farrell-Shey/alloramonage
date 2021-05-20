94efb45e51ad890358f9d8388ea220be66f544d0
Elliot <elliot.decle@gmail.com>
2021-05-20 16:36:41 +0200

    update : merge index

--- public/index.php
+++ public/index.php
@@ -14,22 +14,20 @@ session_start();
 $uri = $_SERVER['REQUEST_URI'];
 $router = new AltoRouter();
 
-$router->map('GET', '/', '/pages/home');
-$router->map('GET', '/conseils', '/pages/conseils', 'conseils');
-// route vers les pages HTML du site - target = 'pages/'
+// route vers les pages HTML du site - target = 'pages/....'
 // renvoie vers les fichiers qui se trouvent dans 'pages/'
-$router->map('GET', '/', 'pages/', 'home');
-$router->map('GET', '/conseils', 'pages/', 'conseils');
+$router->map('GET', '/', 'pages/home' );
+$router->map('GET', '/conseils', 'pages/conseils' );
 $router->map('GET', '/[*:departement]/[*:service]', 'pages/annuaire');
-$router->map('GET', '/annuaire', 'pages/', 'annuaire');
-$router->map('GET', '/prestations', 'pages/', 'prestations');
+$router->map('GET', '/annuaire', 'pages/annuaire' );
+$router->map('GET', '/prestations', 'pages/prestations' );
 
-//route vers l'api rest du site - target = 'functions/'
+//route vers l'api rest du site - target = 'functions/....'
 // renvoie vers les fichiers qui se trouvent dans 'functions/'
-$router->map('POST', '/login', 'functions/', 'login');
-$router->map('POST', '/logout', 'functions/', 'logout');
-$router->map('POST', '/inscription', 'functions/', 'inscription');
-$router->map('POST', '/verification_email', 'functions/', 'verification_email');
+$router->map('POST', '/login', 'functions/login');
+$router->map('POST', '/logout', 'functions/logout');
+$router->map('POST', '/inscription', 'functions/inscription');
+$router->map('POST', '/verification_email', 'functions/verification_email');
 
 $router->map('GET', '/search', 'functions/', 'search');
 /* PARTIE ADMINISTRATION */
@@ -49,7 +47,7 @@ $_SESSION['match'] = $match = $router->match();
 // si $match n'est pas un tableau alors l'url ne correspond à aucune route existante donc direction la page 404
 if (is_array($match)) {
 
-    require "../" . $match['target'] . ( isset($match['name']) ? $match['name'] : null ) . ".php";
+    require "../" . $match['target'] . ".php";
 
 } else {
 
