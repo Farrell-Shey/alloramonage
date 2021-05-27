96715bd3ba50dcda131434b3fdda5b19b424a97b
Killian <killianbchr@gmail.com>
2021-05-27 13:54:14 +0200

    Removed name

--- pages/administration/activites.php
+++ pages/administration/activites.php
@@ -1,5 +1,7 @@
 <?php
 
+$metatitle = "Panneau admin - Activités";
+
 require("../elements/header_administration.php");
 require('../functions/administration/activites.php');
 
@@ -8,11 +10,12 @@ require('../functions/administration/activites.php');
 <h3>Ajouter une activité :</h3>
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title"><input type="text" class="form-control ajoutervalue" id="ajouter" aria-describedby="text" placeholder="Nom de l'activité"></h5>
+    <h5 class="card-title"><input type="text" class="form-control ajoutervalue" aria-describedby="text" placeholder="Nom de l'activité"></h5>
     <a class="btn btn-primary ajouter">Ajouter</a>
   </div>
 </div>
 <br />
+
 <h3>Modifier ou supprimer les activités :</h3>
 
 <?php
@@ -20,7 +23,7 @@ $activites = getActivites();
 foreach ($activites as $activite) {
   $card_activite = '<div class="card" style="width: 18rem;">
                       <div class="card-body">
-                        <h5 class="card-title"><input type="text" class="' . $activite['id'] . ' form-control" id="text" name="' . $activite['id'] . '" aria-describedby="text" placeholder="' . $activite['name'] . '"></h5>
+                        <h5 class="card-title"><input type="text" class="' . $activite['id'] . ' form-control" id="text" aria-describedby="text" placeholder="' . $activite['name'] . '"></h5>
                         <a id="' . $activite['id'] . '" class="btn btn-primary modifier">Modifier</a>
                         <a id="' . $activite['id'] . '" "href="#" class="btn btn-primary supprimer">Supprimer</a>
                       </div>
