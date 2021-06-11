4d6b2b5acf7731cfb2b9f91dd907aa9c4957b3b3
Killian <killianbchr@gmail.com>
2021-06-11 11:47:36 +0200

    Administration - Form revendeur

--- pages/administration/commentaires_a_valider.php
+++ pages/administration/commentaires_a_valider.php
@@ -1,13 +1,26 @@
 <?php
 
-require ("../elements/header_administration.php");
-require ('../functions/administration/commentaires_a_valider.php');
+require("../elements/header_administration.php");
+require('../functions/administration/commentaires_a_valider.php');
 
 $commentaires = getCommentairesAValider();
 
-if($commentaires == null){
+if ($commentaires == null) {
     echo "Aucun commentaires à valider.";
 }
+
+$tableau = '<table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Prénom</th>
+                        <th scope="col">Commentaire</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Utilisateur</th>
+                    </tr>
+                </thead>
+                <tbody>';
 
 foreach ($commentaires as $commentaire) {
     print_r($commentaire);
@@ -15,35 +28,24 @@ foreach ($commentaires as $commentaire) {
 
 ?>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Nom</th>
-      <th scope="col">Prénom</th>
-      <th scope="col">Commentaire</th>
-      <th scope="col">Date</th>
-      <th scope="col">Utilisateur</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+
+<tr>
+    <th scope="row">1</th>
+    <td>Mark</td>
+    <td>Otto</td>
+    <td>@mdo</td>
+</tr>
+<tr>
+    <th scope="row">2</th>
+    <td>Jacob</td>
+    <td>Thornton</td>
+    <td>@fat</td>
+</tr>
+<tr>
+    <th scope="row">3</th>
+    <td>Larry</td>
+    <td>the Bird</td>
+    <td>@twitter</td>
+</tr>
+</tbody>
 </table>
--- pages/administration/revendeurs.php
+++ pages/administration/revendeurs.php
@@ -36,12 +36,12 @@ require('../functions/administration/revendeurs/revendeurs.php');
             $tableau .= '<th><button type="button" class="btn btn-primary modification" id="' . $revendeur['id'] . '" onclick="document.getElementById(\'registration\').classList.toggle(\'d-none\');">Modifier</button></th>';
             $tableau .= '</tr>';
         }
-        echo $tableau;
+        //echo $tableau;
         ?>
     </tbody>
 </table>
 
-<div id="registration" class="filtre-over d-none" onclick="this.classList.toggle('d-none');">
+<div id="registration" class="filtre-over" onclick="this.classList.toggle('d-none');">
     <aside class="modal-registration" onclick="document.getElementById('registration').classList.toggle('d-none');">
         <div class="row">
 
@@ -53,81 +53,98 @@ require('../functions/administration/revendeurs/revendeurs.php');
                     <div class="row">
                         <div class="col-6">
                             <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="mail_login" name="mail_login" placeholder="name@example.com" required>
-                                <label for="mail_login">ID</label>
+                                <input class="form-control" id="id_revendeur" name="id_revendeur" placeholder="0" readonly>
+                                <label for="id_revendeur">ID</label>
+                            </div>
+                        </div>
+                        <div class="col-6" style="padding-left: 20px">
+                            <!-- <div class="form-floating mb-3"> -->
+                            <select class="form-select" id="statut_revendeur" name="statut_revendeur">
+                                <option id="valide" value="1">Validé</option>
+                                <option id="payant" value="10">Payant</option>
+                                <option id="avalider" value="0">À valider</option>
+                                <option id="attente" value="2">En attente</option>
+                                <option id="supprime" value="9">Supprimé</option>
+                            </select>
+                            <!-- </div> -->
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="date_revendeur" name="date_revendeur" placeholder="2020-01-01 12:00:00" readonly>
+                                <label for="date_revendeur">Date</label>
                             </div>
                         </div>
                         <div class="col-6" style="padding-left: 20px">
                             <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="*******" required>
-                                <label for="password_login">Statut</label>
+                                <input class="form-control" id="date_boost_revendeur" name="date_boost_revendeur" placeholder="0000-00-00 00:00:00" required>
+                                <label for="date_boost_revendeur">Date du boost de la position</label>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-6">
                             <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="mail_login" name="mail_login" placeholder="name@example.com" required>
-                                <label for="mail_login">Date</label>
+                                <input class="form-control" id="top_position_revendeur" name="top_position_revendeur" placeholder="21, 22, 23" required>
+                                <label for="top_position_revendeur">Top position</label>
                             </div>
                         </div>
                         <div class="col-6" style="padding-left: 20px">
                             <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="*******" required>
-                                <label for="password_login">Date du boost de la position</label>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="a_paye_revendeur" name="a_paye_revendeur">
+                                    <label class="form-check-label" for="a_paye_revendeur">A payé</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="boost_position_revendeur" name="boost_position_revendeur">
+                                    <label class="form-check-label" for="boost_position_revendeur">Boost position</label>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="mail_login" name="mail_login" placeholder="name@example.com" required>
-                                <label for="mail_login">Top position</label>
-                            </div>
-                        </div>
-                        <div class="col-6" style="padding-left: 20px">
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="*******" required>
-                                <label for="password_login">Boost position</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="mail_login" name="mail_login" placeholder="name@example.com" required>
-                                <label for="mail_login">Motif refus</label>
-                            </div>
-                        </div>
-                        <div class="col-6" style="padding-left: 20px">
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="*******" required>
-                                <label for="password_login">Société</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="mail_login" name="mail_login" placeholder="name@example.com" required>
-                                <label for="mail_login">Contact</label>
-                            </div>
-                        </div>
-                        <div class="col-6" style="padding-left: 20px">
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="*******" required>
-                                <label for="password_login">Adresse</label>
-                            </div>
-                        </div>
+                    <div class="form-group">
+                        <textarea class="form-control" id="refus_revendeur" name="refus_revendeur" rows="3" placeholder="Motif refus"></textarea>
                     </div>
                     <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="confirm_password_inscription" name="confirm_password_inscription" placeholder="*******" required>
-                        <label for="confirm_password_inscription">Code Postal</label>
+                        <input class="form-control" id="societe_renvendeur" name="societe_renvendeur" placeholder="Nom société" required>
+                        <label for="societe_renvendeur">Société</label>
                     </div>
                     <div class="form-floating mb-3">
-                        <input type="text" pattern="[0-9]{9}" class="form-control" id="siren" name="siren" placeholder="siren" required>
-                        <label for="siren">Ville</label>
+                        <input class="form-control" id="contact_revendeur" name="contact_revendeur" placeholder="Nom revendeur" required>
+                        <label for="contact_revendeur">Contact</label>
                     </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="ville_revendeur" name="ville_revendeur" placeholder="Ville revendeur" required>
+                                <label for="ville_revendeur">Ville</label>
+                            </div>
+                        </div>
+                        <div class="col-6" style="padding-left: 20px">
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="code_postal_revendeur" name="code_postal_revendeur" placeholder="00000" required>
+                                <label for="code_postal_revendeur">Code Postal</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                        <div class="form-floating mb-3">
+                        <input class="form-control" id="adresse_revendeur" name="adresse_revendeur" placeholder="Adresse revendeur" required>
+                        <label for="adresse_revendeur">Adresse</label>
+                    </div>
+                        </div>
+                        <div class="col-4" style="padding-left: 20px">
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="pays_revendeur" name="pays_revendeur" placeholder="Pays revendeur" required>
+                                <label for="pays_revendeur">Pays</label>
+                            </div>
+                        </div>
+                    </div>
+                   
+
+
                 </form>
             </div>
 
@@ -141,69 +158,55 @@ require('../functions/administration/revendeurs/revendeurs.php');
                     <div class="row">
                         <div class="col-6">
                             <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="mail_login" name="mail_login" placeholder="name@example.com" required>
-                                <label for="mail_login">Téléphone</label>
+                                <input class="form-control" id="telephone_revendeur" name="telephone_revendeur" placeholder="0000000000" required>
+                                <label for="telephone_revendeur">Téléphone</label>
                             </div>
                         </div>
                         <div class="col-6" style="padding-left: 20px">
                             <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="*******" required>
-                                <label for="password_login">Fax</label>
+                                <input class="form-control" id="fax_revendeur" name="fax_revendeur" placeholder="*******" required>
+                                <label for="fax_revendeur">Fax</label>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-6">
                             <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="mail_login" name="mail_login" placeholder="name@example.com" required>
-                                <label for="mail_login">E-mail</label>
+                                <input type="email" class="form-control" id="email_revendeur" name="email_revendeur" placeholder="name@example.com" required>
+                                <label for="email_revendeur">E-mail</label>
                             </div>
                         </div>
                         <div class="col-6" style="padding-left: 20px">
                             <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="*******" required>
-                                <label for="password_login">URL</label>
+                                <input class="form-control" id="url_revendeur" name="url_revendeur" placeholder="example.com" required>
+                                <label for="url_revendeur">URL</label>
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-6">
                             <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="mail_login" name="mail_login" placeholder="name@example.com" required>
-                                <label for="mail_login">Tarif</label>
+                                <input class="form-control" id="tarif_revendeur" name="tarif_revendeur" placeholder="000" required>
+                                <label for="tarif_revendeur">Tarif</label>
                             </div>
                         </div>
                         <div class="col-6" style="padding-left: 20px">
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password_login" name="password_login" placeholder="*******" required>
-                                <label for="password_login">Description</label>
-                            </div>
+                            <label for="certificat_revendeur">Certificat</label>
+                            <select class="form-select" id="certificat_revendeur" name="certificat_revendeur">
+                                <option id="oui" value="1">Oui</option>
+                                <option id="non" value="0">Non</option>
+                                <option id="nonprecise" value="2">Non précisé</option>
+                            </select>
                         </div>
                     </div>
-
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="form-floating mb-3">
-                                <input type="text" pattern="[0-9]{5}" class="form-control" id="code_postal" name="code_postal" placeholder="code postal" required>
-                                <label for="code_postal">Certificat</label>
-                            </div>
-                        </div>
-                        <div class="col-6" style="padding-left: 20px">
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="ville" name="ville" placeholder="ville" required>
-                                <label for="ville">Activités</label>
-                            </div>
-                        </div>
+                    <div class="form-group">
+                        <textarea class="form-control" id="description_revendeur" name="description_revendeur" rows="3" placeholder="Description revendeur"></textarea>
                     </div>
-
                     <div class="form-floating mb-3">
-                        <input type="tel" pattern="[0]{1}[0-9]{9}" class="form-control" id="telephone" name="telephone" placeholder="0000000000" required>
-                        <label for="telephone">Description</label>
+                        <input type="text" class="form-control" id="activites_revendeurs" name="activites_revendeurs" placeholder="Activités revendeur" required>
+                        <label for="activites_revendeurs">Activités</label>
                     </div>
-                    <div class="form-floating">
-                        <input type="text" class="form-control" id="societe" name="societe" placeholder="societe" required>
-                        <label for="societe">Certificat délivré</label>
-                    </div>
+
                     <button name="inscription" id="inscription" class="btn btn-outline-primary" type="submit">ENREGISTRER LES MODIFICATIONS</button>
                 </form>
             </div>
